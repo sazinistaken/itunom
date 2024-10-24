5668a14f5557853c5e5538df0496f92df357cf4a
Salih Çelik <acelik2022hotmail.com>
2024-10-24 20:58:43 +0300

    Added queue for IPC

--- itunom.py
+++ itunom.py
@@ -1,8 +1,11 @@
 import math
 import threading
 import time
+import queue
 
 DEGREE_SYMBOL = '\u00B0'
+
+position_queue = queue.Queue()
 
 class Vehicle():
     def __init__(self, name: str, x, y, velocity, direction, altidute=0):
@@ -27,7 +30,8 @@ class Vehicle():
 
 
     def get_position(self):
-        print(f"[{self.name}] Position: ({self.x}, {self.y}), Velocity: {self.velocity}, Direction: {self.direction}{DEGREE_SYMBOL}, Altitude: {self.altidute}")
+        position = self.name, self.x, self.y, self.velocity, self.direction, self.altidute
+        position_queue.put(position)
     
 
 
@@ -58,23 +62,46 @@ class VehicleThread (threading.Thread):
     def stop(self):
         self.running = False
 
+class PositionLogThread(threading.Thread):
+    def __init__(self):
+        super().__init__()
+        self.running =  True
+
+    def run(self):
+        while self.running:
+            try:
+                position = position_queue.get(timeout=1.0)
+                print(f"[{position[0]}] Position: ({position[1],position[2]}), Velocity: {position[3]}, Direction: {position[4]}{DEGREE_SYMBOL}, Altidute: {position[5]}")
+                position_queue.task_done()
+            except queue.Empty:
+                continue
+
+    def stop(self):
+        self.running = False
+
 
 def main():
     quadcopter = Quadcopter("Quadcopter", 0, 0, 10, 90, 100)
     quadcopter_thread = VehicleThread(quadcopter)
-    quadcopter_thread.start()
 
     drone = Drone("Drone", 0, 5, 20, 180, 100)
     drone_thread = VehicleThread(drone)
-    drone_thread.start()
 
+    log_thread =  PositionLogThread()
+
+    drone_thread.start()
+    quadcopter_thread.start()
+    log_thread.start()
+    
     time.sleep(5.0)
 
     quadcopter_thread.stop()
     drone_thread.stop()
-    #quadcopter_thread.join()
-    #drone_thread.join()
+    log_thread.stop()
 
+    quadcopter_thread.join()
+    drone_thread.join()
+    log_thread.join()
 
 if __name__ == "__main__":
     main()
